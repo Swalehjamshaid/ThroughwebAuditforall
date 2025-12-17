b35b17dcd1731eea85949ab4597d6a63ed8393d0
Swalehjamshaid <roy.jamshaid@gmail.com>
2025-12-17 14:31:07 +0500

    Create audit_service.py

---
+++ app/app/audit_service.py
@@ -0,0 +1,10 @@
+import httpx
+from bs4 import BeautifulSoup
+
+class WebAuditor:
+    def __init__(self, url):
+        self.url = url
+    
+    def execute_50_metrics(self):
+        # Technical, SEO, Content, UX, Performance, Security, Compliance
+        pass
