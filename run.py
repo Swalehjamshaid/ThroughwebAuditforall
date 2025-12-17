f3cb7a7a4b4589d3c20219f186cbcf2877f5591a
Swalehjamshaid <roy.jamshaid@gmail.com>
2025-12-17 15:35:20 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
-from app.app.app import create_app
 import os
+from app.app.app.app import create_app
 
+# Create the application instance
 app = create_app()
 
 if __name__ == "__main__":
+    # Use the PORT provided by Railway, default to 8080
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
