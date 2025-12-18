db0d6ecc1b58b7f2187e383e4e0212c07a06c877
Swalehjamshaid <roy.jamshaid@gmail.com>
2025-12-18 15:54:53 +0500

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-
-# run.py (at repository root /app)
-import os
-from app import create_app  # create_app is defined in top-level app/__init__.py
-
-app = create_app()
-
-if __name__ == "__main__":
-       port = int(os.environ.get("PORT", 8080))
