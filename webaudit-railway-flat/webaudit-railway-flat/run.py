504ccca7a6163b218860ada0488e38faacf58b0e
Swalehjamshaid <roy.jamshaid@gmail.com>
2025-12-18 18:32:16 +0500

    Update run.py

--- webaudit-railway-flat/webaudit-railway-flat/run.py
+++ webaudit-railway-flat/webaudit-railway-flat/run.py
@@ -1,10 +1,11 @@
 
-# run.py
+import os
 from app import create_app
 
+# Create the Flask application object for Gunicorn to import
 app = create_app()
 
+# Local dev only. Under Gunicorn, this block is ignored.
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.
+    app.run(host="0.0.0.0", port=port)
