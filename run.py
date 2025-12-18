52f2818193708f5059843cc9578a201299aa87f1
Swalehjamshaid <roy.jamshaid@gmail.com>
2025-12-18 09:24:07 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-# Ensure the app folder is recognized as a package
+# Ensure the app folder is recognized as a package for imports
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 from app import create_app
