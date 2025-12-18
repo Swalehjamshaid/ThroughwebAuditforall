bbf246396f59ebf28fea3d92385fda5b68a58d0f
Swalehjamshaid <roy.jamshaid@gmail.com>
2025-12-18 10:10:44 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 import os
 import sys
 
-# Get the absolute path of the root
+# Get the absolute path of the project root
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, BASE_DIR)
 
-# Add the nested app folders to the path so 'models' and 'auth' can be found
-# Based on your structure: ThroughwebAuditforall/app/app/
-nested_app_path = os.path.join(BASE_DIR, 'app', 'app')
-sys.path.insert(0, nested_app_path)
+# Add the nested folders to the Python path so 'models' can be found
+# Path: /app/app/
+nested_path = os.path.join(BASE_DIR, 'app', 'app')
+sys.path.insert(0, nested_path)
 
 from app.app import create_app
 
