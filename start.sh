87b669772b375efcf4cf27c5003734170551e7ad
Swalehjamshaid <roy.jamshaid@gmail.com>
2025-12-28 19:34:55 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,1 @@
-
-#!/usr/bin/env sh
-set -e
-
-# If the platform doesn't provide PORT, default to 8080
-PORT="${PORT:-8080}"
-
-# Start your FastAPI app
-exec uvicorn fftech_audit.app:app --host 0.0.0.0 --port "$PORT"
-``
+CMD ["./start.sh"]
