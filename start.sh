86efe5fc18fb40f1d6773980e62a5325d818ee47
Swalehjamshaid <roy.jamshaid@gmail.com>
2025-12-28 19:28:09 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 
+# start.sh
 #!/usr/bin/env sh
 set -e
 
-# Default if the platform doesn't inject PORT
+# Use platform-provided PORT or default to 8080
 PORT="${PORT:-8080}"
 
 exec uvicorn fftech_audit.app:app --host 0.0.0.0 --port "$PORT"
