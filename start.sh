ea75861206d5d7d32183323e3b17d52019f7e65c
Swalehjamshaid <roy.jamshaid@gmail.com>
2025-12-29 11:16:45 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 #!/usr/bin/env sh
 set -e
 PORT="${PORT:-8080}"
-exec uvicorn fftech_audit.app:app --host 0.0.0.0 --port "$PORT"
+exec uvicorn fftech_audit.app:app --host 0.0.0.0 --port "$PORT" --log-level info
