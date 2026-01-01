4dbbee2f1623f0bb739751e9eead9bcb2b1b0921
Swalehjamshaid <roy.jamshaid@gmail.com>
2026-01-01 17:29:14 +0500

    Delete File directory

--- File/app/settings.py
+++
@@ -1,8 +0,0 @@
-
-import os
-SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key-change-me")
-ADMIN_EMAIL = os.getenv("ADMIN_EMAIL", "admin@fftech.app")
-ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "Admin@123")
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///data.db")
-BRAND_NAME = "FF Tech"
-REPORT_VALIDITY_DAYS = 30
