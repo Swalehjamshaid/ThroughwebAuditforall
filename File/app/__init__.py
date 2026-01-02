66662b7cd442ce888aabff12429b685b52952c62
Swalehjamshaid <roy.jamshaid@gmail.com>
2026-01-02 18:48:34 +0500

    Update __init__.py

--- File/app/__init__.py
+++ File/app/__init__.py
@@ -1,3 +1,22 @@
-
 # app/__init__.py
-# Marks this directory as a Python package.
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
+from apscheduler.schedulers.background import BackgroundScheduler
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+mail = Mail(app)
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+from app import routes, models, tasks
