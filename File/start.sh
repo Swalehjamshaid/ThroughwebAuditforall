c0015dc1ef222523282d136b1c8dc3f7c326e602
Swalehjamshaid <roy.jamshaid@gmail.com>
2026-01-03 13:58:41 +0500

    Delete File directory

--- File/start.sh
+++
@@ -1,9 +0,0 @@
-
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "Starting Gunicorn..."
-echo "PORT=${PORT:-8080}  WORKERS=${WORKERS:-1}"
-
-# If your main.py is at app/app/app/main.py, change module path below accordingly.
-exec gunicorn app.app.main:app --bind 0.0.0.0:${PORT:-8080} --workers ${WORKERS:-1}
