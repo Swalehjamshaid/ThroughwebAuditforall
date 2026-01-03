17a45d92913d9afc1582b1a7b0da4e9f6b1c9052
Swalehjamshaid <roy.jamshaid@gmail.com>
2026-01-03 16:18:49 +0500

    Delete File directory

--- File/start.sh
+++
@@ -1,7 +0,0 @@
-
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "Starting Gunicorn..."
-echo "PORT=${PORT:-8080}  WORKERS=${WORKERS:-1}"
-exec gunicorn app.app.main:app --bind 0.0.0.0:${PORT:-8080} --workers ${WORKERS:-1}
