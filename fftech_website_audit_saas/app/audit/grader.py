ccd20091d590d77c4cef44ac7bd2ae1d326a9849
Swalehjamshaid <roy.jamshaid@gmail.com>
2026-01-06 12:37:30 +0500

    Add files via upload

---
+++ fftech_website_audit_saas/app/audit/grader.py
@@ -0,0 +1,8 @@
+def compute_overall(d):
+    return sum(d.values())//len(d)
+
+def grade_from_score(s):
+    return 'A'
+
+def summarize_200_words(u,d,i):
+    return 'Summary'
---
+++ fftech_website_audit_saas/app/audit/report.py
@@ -0,0 +1,2 @@
+def render_pdf(path,brand,url,grade,score,cs,summary):
+    open(path,'wb').write(b'%PDF-1.4 dummy')
